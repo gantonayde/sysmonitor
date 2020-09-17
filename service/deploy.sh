a37fca3e0d6d0e34d7902482f254139878ab25fa
Genadi Naydenov <gnaydenov19@gmail.com>
2020-09-17 17:06:08 +0100

    Beta

--- service/deploy.sh
+++ service/deploy.sh
@@ -1,9 +1,51 @@
-SYSMONITOR_DIR=`pwd`
-SERVICE_NAME='sysmonitor_test'
+#!/bin/bash
 
-sudo SYSMONITOR_DIR=${SYSMONITOR_DIR} envsubst < service/sysmonitor_test.service > ${SERVICE_NAME}.service
-sudo mv sysmonitor_test.service /lib/systemd/system/${SERVICE_NAME}.service
-sudo chmod 644 /lib/systemd/system/${SERVICE_NAME}.service
-sudo systemctl daemon-reload
-sudo systemctl enable ${SERVICE_NAME}.service
+SYSMONITOR_DIR=`pwd`
+USERNAME=`whoami`
+SERVICE_NAME='sysmonitor_example'
+RC_LOCAL_DIR='/etc'
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case "$key" in
+        -r|--remove)
+        REMOVE=true
+        ;;
+        -rc|--rclocal)
+        RC=true
+        ;;
+    esac
+    shift
+done
+
+if [ $REMOVE ] && ! [ $RC ] ; then
+    sudo systemctl stop ${SERVICE_NAME}.service 
+    sudo rm /lib/systemd/system/${SERVICE_NAME}.service
+    sudo unlink /etc/systemd/system/multi-user.target.wants/${SERVICE_NAME}.service
+    sudo systemctl daemon-reload
+    echo "${SERVICE_NAME}.py is removed."
+elif [ $RC ] && ! [ $REMOVE ]; then
+    if grep -Fxq "sudo python3 ${SYSMONITOR_DIR}/${SERVICE_NAME}.py & > ${SYSMONITOR_DIR}/log.txt 2>&1" service/rc.local; then
+        echo "${SERVICE_NAME}.py is already installed."
+    else
+        sudo sed -i "/^exit 0/i sudo python3 ${SYSMONITOR_DIR}/${SERVICE_NAME}.py & > ${SYSMONITOR_DIR}/log.txt 2>&1" ${RC_LOCAL_DIR}/rc.local
+        echo "${SERVICE_NAME}.py installed. Please reboot the system."
+    fi    
+elif [ $RC ] && [ $REMOVE ]; then
+    sudo grep -v "sudo python3 ${SYSMONITOR_DIR}/${SERVICE_NAME}.py & > ${SYSMONITOR_DIR}/log.txt 2>&1" ${RC_LOCAL_DIR}/rc.local > ${RC_LOCAL_DIR}/rc.local2;
+    sudo mv ${RC_LOCAL_DIR}/rc.local2 ${RC_LOCAL_DIR}/rc.local
+    echo "${SERVICE_NAME}.py removed. Please reboot the system."
+else
+    sudo SYSMONITOR_DIR=${SYSMONITOR_DIR} USERNAME=${USERNAME} SERVICE_NAME=${SERVICE_NAME} envsubst < service/sysmonitor_test.service > ${SERVICE_NAME}.service
+    sudo mv ${SERVICE_NAME}.service /lib/systemd/system/${SERVICE_NAME}.service
+    sudo chmod 644 /lib/systemd/system/${SERVICE_NAME}.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable ${SERVICE_NAME}.service
+    sudo systemctl start ${SERVICE_NAME}.service   
+    echo "${SERVICE_NAME}.py installed as a service."
+    echo "You can check its status with:"
+    echo "sudo systemctl status ${SERVICE_NAME}.service" 
+fi
+
+
 
--- test.py
+++ test.py
@@ -1,28 +1,31 @@
-import unittest
-from sysmonitor import get_mem_load, get_proc_by_memory, get_proc_details
+#!/usr/bin/python3
 
+import unittest
+from sysmonitor import get_mem_load, get_proc_by_memory, get_details_with_shell
 
 class TestSysmonitor(unittest.TestCase):
 
     def test_mem_load(self):
         """
         Test that memory load can be measured.
-        """ 
+        """
         self.assertTrue(get_mem_load())
 
     def test_proc_list(self):
         """
         Test that a list of processes can be obtained.
-        """ 
-        self.assertTrue(len(get_proc_by_memory().index.values))
-    
-    def test_proc_details(self):
         """
-        Test that process details can be obtained.
-        """ 
-        self.assertTrue(get_proc_details(get_proc_by_memory().index.values[0]))
+        self.assertTrue(len(get_proc_by_memory().index.values))
+
+    def test_get_details_with_shell(self):
+        """
+        Test that process shell commands can be executed.
+        Test if returned object is an instance of a string.
+        """
+        test_cmd = 'echo Hello'
+        self.assertTrue(get_details_with_shell(test_cmd))
+        self.assertIsInstance(get_details_with_shell(test_cmd), str)
 
 if __name__ == '__main__':
     unittest.main()
-
 
